1e1b85d90f071cddbb76790eeb60fa0929477657
chris <chrisshields88@gmail.com>
2018-02-01 05:58:23 -0500

    update

---
+++ Module_7/Section 9 - Assignment 8/Start/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <br>
+                <button @click="switchComponent = 'app-blue'" class="btn btn-primary">Load Blue Template</button>
+                <button @click="switchComponent = 'app-green'" class="btn btn-success">Load Green Template</button>
+                <button @click="switchComponent = 'app-red'" class="btn btn-danger">Load Red Template</button>
+                <hr>
+                <component :is="switchComponent">
+                    <slot></slot>                    
+                </component>   
+               <!--  <app-blue>
+                    <h2>This is the Heading</h2>
+                    <p>This is the content</p>
+                </app-blue>
+                <app-green>
+                    <h2>This is the Heading</h2>
+                    <p>This is the content</p>
+                </app-green>
+                <app-red>
+                    <h2>This is the Heading</h2>
+                    <p>This is the content</p>
+                </app-red> -->
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Blue from './components/Blue.vue';
+import Green from './components/Green.vue';
+import Red from './components/Red.vue';
+
+export default {
+
+    components: {
+        appBlue: Blue,
+        appGreen: Green,
+        appRed: Red
+    }
+}
+</script>
+
+<style>
+</style>
---
+++ Module_7/Section 9 - Assignment 8/Start/src/components/Green.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<slot></slot>
+	</div>
+</template>
+
+<script>
+export default{
+	data(){
+		return{
+			heading: 'This is the heading',
+			subtitle: 'This is the subtitle'
+		}
+	}
+}
+
+</script>
+
+<style scoped>
+div {
+	border: 1px solid green;
+	background-color: lightgreen;
+	padding: 30px;
+	margin: 20px auto;
+	text-align: center
+}
+</style>

32ebab0ae9e39d63e9b616a86822845addd76f4b
chris <chrisshields88@gmail.com>
2018-02-01 20:13:47 -0500

    Update
    Added Components (NewQuote, QuoteGrid, Quotes)
    Added functionality to display quotes
    Added Methods To Add a New Quote

--- Module_7/Project-Quote-Machine/src/App.vue
+++ Module_7/Project-Quote-Machine/src/App.vue
@@ -1,13 +1,31 @@
 <template>
-    <div class="container">
-    
-    </div>
+	<div class="container">
+		<new-created-quote @quoteAdded="newQuote"></new-created-quote>
+		<app-quote-grid :quotes="quotes"></app-quote-grid>
+	</div>
 </template>
 
 <script>
-    export default {
-        
-    }
+import QuoteGrid from './components/QuoteGrid.vue';
+import NewQuote from './components/NewQuote.vue';
+export default {
+	data (){
+		return{
+			quotes: ['Just a quote to see something'],
+			maxQuotes: 10
+		}
+	},
+	methods:{
+		newQuote(quote){
+			this.quotes.push(quote);
+		}
+	},
+	components:{
+		appQuoteGrid: QuoteGrid,
+		newCreatedQuote: NewQuote
+	}
+
+}
 </script>
 
 <style>

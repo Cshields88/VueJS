3e2e2fcdc0821c718cbce7dec85f5fbca0107abf
chris <chrisshields88@gmail.com>
2018-02-08 11:11:00 -0500

    Update
    Worked On Assignment 9

---
+++ Module_8/assignment-9/src/Data.vue
@@ -0,0 +1,41 @@
+<template>
+	<div>
+		<div 
+		id="yes"
+		@click="switched(true)"
+		:class="{active: value}">Yes</div>
+		<div 
+		id="no"
+		@click="switched(false)"
+		:class="{active: !value}">No</div>
+	</div>
+</template>
+<script>
+export default{
+	props: ['value'],
+	methods:{
+		switched(isOn){
+			this.$emit('input', isOn)
+		}
+	}
+}
+</script>
+<style sccoped>
+#yes, #no{
+	width: 40px;
+	height: 20px;
+	background-color: lightgrey;
+	padding: 2px;
+	display: inline-block;
+	margin: 10px -2px;
+	box-sizing: content-box;
+	text-align: center;
+	cursor:pointer;
+}
+#yes:hover, #yes.active{
+	background-color: lightgreen;
+}
+#no:hover, #no.active{
+	background-color: lightcoral;
+}
+</style>

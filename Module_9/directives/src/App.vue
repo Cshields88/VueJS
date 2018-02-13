e0997035dbcac0c722b3b2e3a50d8cf816a519ae
chris <chrisshields88@gmail.com>
2018-02-13 06:53:07 -0500

    update
    coursework on custom directives

--- Module_9/directives/src/App.vue
+++ Module_9/directives/src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <p v-highlight v-text-color>hello</p>
+    <h1>Built-In Components</h1>
+    <p v-text="'This is some text'"></p>
+    <p v-html="'<strong>This is some bold text</strong>'"></p>
+    <hr>
+    <h1>Custom Components</h1>
+    <p v-highlight="'purple'">hello</p>
   </div>
 </template>
 

b2f306b5a2b658ffbc6a230a39eab9440236a4f6
chris <chrisshields88@gmail.com>
2018-02-14 06:57:14 -0500

    Course work - lessons on creating custom directives

--- Module_9/directives/src/App.vue
+++ Module_9/directives/src/App.vue
@@ -5,13 +5,49 @@
     <p v-html="'<strong>This is some bold text</strong>'"></p>
     <hr>
     <h1>Custom Components</h1>
-    <p v-highlight="'purple'">hello</p>
+    <p v-highlight:background.delayed="'purple'">hello</p>
+    <p v-local-highlight:background.delayed.blink="'green'">hello</p>
   </div>
 </template>
 
 <script>
 export default {
+  directives: { //Registers local directive
+    'local-highlight': {
+      bind(el, binding, vnode){
+    // el.style.backgroundColor = 'green';
+    // el.style.backgroundColor = binding.value;
+    var delay = 0;
+    if(binding.modifiers['delayed']){
+      delay = 3000;
+    }
+    if(binding.modifiers['blink']){
+      let mainColor = binding.value;
+      let secondColor = 'blue';
+      let currentColor = mainColor;
+      setTimeout(() => {
+        setInterval(() => {
+          currentColor == secondColor ? currentColor = mainColor : currentColor = secondColor;
+          if(binding.arg == 'background'){
+            el.style.backgroundColor = currentColor;
+          }else{
+            el.style.color = currentColor;
+          }
+        },1000);
 
+      }, delay)
+    }
+    setTimeout(() => {
+      if(binding.arg == 'background'){
+        el.style.backgroundColor = binding.value;
+      }else{
+        el.style.color = binding.value;
+      }
+    }, delay)
+    
+  }
+}
+}
 }
 </script>
 

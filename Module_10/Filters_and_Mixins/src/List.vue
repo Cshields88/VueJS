280b06b1246abebd1fc76ed2b7585326841d1b23
chris <chrisshields88@gmail.com>
2018-02-19 07:10:12 -0500

    Course Work
    Added list Component registered it in app.vue

--- Module_10/Filters_and_Mixins/src/List.vue
+++ Module_10/Filters_and_Mixins/src/List.vue
@@ -1,29 +1,17 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Filters & Mixins</h1>
-                <!-- {{Data name | Filter Name}} -->
-                <p>{{text | toUppercase | to-lowercase}}</p> 
-                <hr>
+   <div>
+       <h1>Filters & Mixins</h1>
                 <input type="text" v-model="filterText">
                 <ul>
                     <li v-for="fruit in filteredFruits">{{fruit}}</li>
                 </ul>
-                <hr>
-                <app-list></app-list>
-            </div>
-        </div>
-    </div>
+   </div>
 </template>
 
 <script>
-import List from './List.vue';
-
     export default {
         data(){
             return{
-                text: 'Hello There',
                 fruits: ['Apple', 'Orange', 'Banana', 'Melon'],
                 filterText: ''
             }
@@ -39,9 +27,6 @@ import List from './List.vue';
                     return element.match(this.filterText);
                 });
             }
-        },
-        components:{
-            appList: List
         }
     }
 </script>

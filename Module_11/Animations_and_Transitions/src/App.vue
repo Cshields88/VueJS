6b1391a41d7aafd87e113360d26d15e1f3281f85
chris <chrisshields88@gmail.com>
2018-02-21 06:42:54 -0500

    Coursework
    Added fade transition to alert when button is clicked

--- Module_11/Animations_and_Transitions/src/App.vue
+++ Module_11/Animations_and_Transitions/src/App.vue
@@ -3,6 +3,12 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Animations & Transitions</h1>
+                <hr>
+                <button class="btn btn-priimary" @click="show=!show">Show Alert</button>
+                <br><br>
+                <transition name="fade">
+                <div class="alert alert-info" v-if="show">This is an alert!</div>
+                </transition>
 
             </div>
         </div>
@@ -11,9 +17,26 @@
 
 <script>
     export default {
+        data(){
+            return{
+                show: false
+            }
+        }
     }
 </script>
 
 <style>
-
+.fade-enter{
+    opacity: 0;
+}
+.fade-enter-active{
+    transition: opacity 1s;
+}
+.fade-leave{
+    
+}
+.fade-leave-active{
+    transition: opacity 1s;
+    opacity: 0;
+}
 </style>

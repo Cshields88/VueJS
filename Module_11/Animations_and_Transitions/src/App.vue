e8dd43cc106c9ec7aadba024d5cf26d181cf0108
chris <chrisshields88@gmail.com>
2018-02-21 20:24:16 -0500

    Coursework
    Added Animation classes
    Added Classes using Animate.css

--- Module_11/Animations_and_Transitions/src/App.vue
+++ Module_11/Animations_and_Transitions/src/App.vue
@@ -9,7 +9,14 @@
                 <transition name="fade">
                 <div class="alert alert-info" v-if="show">This is an alert!</div>
                 </transition>
-                <transition name="slide">
+                <transition name="slide" type="animation">
+                <div class="alert alert-info" v-if="show">This is an alert!</div>
+                </transition>
+                <transition  
+                appear-active-class="animated jello"        
+                 enter-active-class="animated bounce"               
+                 leave-active-class="animated shake"
+                >
                 <div class="alert alert-info" v-if="show">This is an alert!</div>
                 </transition>
 
@@ -22,7 +29,7 @@
     export default {
         data(){
             return{
-                show: false
+                show: true
             }
         }
     }
@@ -43,16 +50,19 @@
     opacity: 0;
 }
 .slide-enter{
-
+    opacity: 0;
 }
 .slide-enter-active{
     animation: slide-in 1s ease-out forwards;
+    transition: opacity 0.5s;
 }
 .slide-leave{
     
 }
 .slide-leave-active{
     animation: slide-out 1s eas-out forwards;
+    transition: opacity 3s;
+    opacity: 0;
 }
 @keyframes slide-in{
     from{

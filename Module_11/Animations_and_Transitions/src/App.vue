063e750602a32a5a51a0d160aebf88e8688c2ca4
chris <chrisshields88@gmail.com>
2018-02-23 14:15:22 -0500

    coursework
    completed lesson on using animations on dynamic components

--- Module_11/Animations_and_Transitions/src/App.vue
+++ Module_11/Animations_and_Transitions/src/App.vue
@@ -41,19 +41,33 @@
                 >
                 <div style="width: 300px; height: 100px; background-color:lightgreen;" v-if="load"></div>
                 </transition>
+                <hr>
+                <h1>Dynamic Components</h1>
+                <br><br>
+                <button class="btn btn-primary"
+                 @click="selectedComponent == 'app-success-alert' ? selectedComponent = 'app-danger-alert' : selectedComponent = 'app-success-alert'"
+                 >Toggle Component</button>
+               <transition name="fade" mode="out-in">
+                  <component :is="selectedComponent"></component>
+               </transition>
+               <br><br>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import DangerAlert from "./DangerAlert.vue";
+import SuccessAlert from "./SuccessAlert.vue";
+
 export default {
   data() {
     return {
       show: false,
       load: true,
       alertAnimation: "fade",
-      elementWidth: 100
+      elementWidth: 100,
+      selectedComponent: "app-success-alert"
     };
   },
   methods: {
@@ -104,6 +118,10 @@ export default {
     leaveCancelled(el) {
       console.log("leaveCancelled");
     }
+  },
+  components: {
+    appDangerAlert: DangerAlert,
+    appSuccessAlert: SuccessAlert
   }
 };
 </script>

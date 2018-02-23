c17207f367ed1b22b2527c5a13f81da527f2876f
chris <chrisshields88@gmail.com>
2018-02-23 08:31:26 -0500

    CourseWork
    Created animation using javascript

--- Module_11/Animations_and_Transitions/src/App.vue
+++ Module_11/Animations_and_Transitions/src/App.vue
@@ -39,7 +39,7 @@
                 @leave-cancelled="leaveCancelled"
                 :css="false"
                 >
-                <div style="width: 100px; height: 100px; background-color:lightgreen;" v-if="load"></div>
+                <div style="width: 300px; height: 100px; background-color:lightgreen;" v-if="load"></div>
                 </transition>
             </div>
         </div>
@@ -52,15 +52,27 @@ export default {
     return {
       show: false,
       load: true,
-      alertAnimation: "fade"
+      alertAnimation: "fade",
+      elementWidth: 100
     };
   },
   methods: {
     beforeEnter(el) {
       console.log("beforeEnter");
+      this.elementWidth = 100;
+      el.style.width = this.elementWidth + "px";
     },
     enter(el, done) {
       console.log("enter");
+      let round = 1;
+      const interval = setInterval(() => {
+        el.style.width = this.elementWidth + round * 10 + "px";
+        round++;
+        if (round > 20) {
+          clearInterval(interval);
+          done();
+        }
+      }, 20);
       done();
     },
     afterEnter(el) {
@@ -70,10 +82,21 @@ export default {
       console.log("enterCancelled");
     },
     beforeLeave(el) {
-      console.log("beforeEnter");
+      console.log("beforeLeave");
+      this.elementWidth = 300;
+      el.style.width = this.elementWidth + "px";
     },
     leave(el, done) {
-      done();
+      console.log("leave");
+      let round = 1;
+      const interval = setInterval(() => {
+        el.style.width = this.elementWidth - round * 10 + "px";
+        round++;
+        if (round > 20) {
+          clearInterval(interval);
+          done();
+        }
+      }, 20);
     },
     afterLeave(el) {
       console.log("afterLeave");

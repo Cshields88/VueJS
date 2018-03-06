a98ae8185a0f72456d7d154d5ffa4dcf11090278
chris <chrisshields88@gmail.com>
2018-03-06 06:30:49 -0500

    initial commit
    added routes for home page and user page

---
+++ Module_13/Routing/src/routes.js
@@ -0,0 +1,13 @@
+import User from "./components/user/User.vue"; //imports component
+import Home from "./components/Home.vue";
+
+export const routes = [
+  {
+    path: "",
+    component: Home
+  },
+  {
+    path: "/user", //this is what is placed at the end of the url
+    component: User //What component needs to be loaded when directed to path
+  }
+];

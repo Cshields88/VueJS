7778f79ec72da2bb4e2a646fa49e3ae385b137ca
chris <chrisshields88@gmail.com>
2018-03-07 07:03:46 -0500

    update
    added id p tag to track the id of current page
    
    added a watcher to update page id when switched

--- Module_13/Routing/src/components/user/User.vue
+++ Module_13/Routing/src/components/user/User.vue
@@ -2,12 +2,23 @@
   <div>
     <h1>User Page</h1>
     <hr>
+    <p>Loaded Id: {{id}}</p>
     <button @click="navigateToHome" class="btn btn-primary">Go To Home</button>
   </div>
 
 </template>
 <script>
 export default {
+  data() {
+    return {
+      id: this.$route.params.id
+    };
+  },
+  watch: {
+    $route(to, from) {
+      this.id = to.params.id;
+    }
+  },
   methods: {
     navigateToHome() {
       this.$router.push("/");

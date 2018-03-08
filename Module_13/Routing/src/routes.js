8583eb4e47b3e90910ba54713a13596bf5af228a
chris <chrisshields88@gmail.com>
2018-03-08 06:27:03 -0500

    commit
    switched routing to dynamically bring in user data

--- Module_13/Routing/src/routes.js
+++ Module_13/Routing/src/routes.js
@@ -1,4 +1,7 @@
 import User from "./components/user/User.vue"; //imports component
+import UserStart from "./components/user/UserStart.vue";
+import UserDetail from "./components/user/UserDetail.vue";
+import UserEdit from "./components/user/UserEdit.vue";
 import Home from "./components/Home.vue";
 
 export const routes = [
@@ -7,7 +10,13 @@ export const routes = [
     component: Home
   },
   {
-    path: "/user/:id", //this is what is placed at the end of the url
-    component: User //What component needs to be loaded when directed to path
+    path: "/user", //this is what is placed at the end of the url
+    //What component needs to be loaded when directed to path
+    component: User,
+    children: [
+      { path: "", component: UserStart },
+      { path: ":id", component: UserDetail },
+      { path: ":id/edit", component: UserEdit }
+    ]
   }
 ];
